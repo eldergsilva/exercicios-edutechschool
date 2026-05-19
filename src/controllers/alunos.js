cf7ef0135b4661ec84c1e8a63d09e5328cb0e7c4
eldergsilva <eldergsilva@gmail.com>
2026-05-19 14:15:09 -0300

    criar aluno

--- src/controllers/alunos.js
+++ src/controllers/alunos.js
@@ -5,17 +5,44 @@ const listarAlunos = (req, res) => {
     return res.status(400).json({mensagem:'A senha é obrigatória!'});
     }
     if(senha_escola !== escola.senha){
-    return res.status(400).json({mensagem:'A senha da escola informada é inválida!'});
+    return res.status(400).json({mgitensagem:'A senha da escola informada é inválida!'});
     }
      
     return res.status(201).json(alunos);
   }
   
  const criarAluno = (req,res)=>{
-    return res.status(201).json('Criar alunos rodando!.')
- }  
+     
 
+    const {nome,cpf,data_nascimento,telefone,email,senha,turma}=req.body;
 
+    if(!nome || !cpf || !data_nascimento || !telefone || !email || !senha || !turma){
+        return res.status(400).json({mensagem:'Todos os campos são obrigatórios!'});
+    }
+    
+    const buscaCpfAluno = alunos.find((aluno) => aluno.cpf === cpf);
+    if(buscaCpfAluno){
+        return res.status(400).json({mensagem:'O CPF informado já está cadastrado!'});
+    }
+    const buscaEmailAluno = alunos.find((aluno) => aluno.email === email);
+    if(buscaEmailAluno){
+        return res.status(400).json({mensagem:'O E-mail informado já está cadastrado!'});
+    }
+    
+    const novoAluno = {
+        id: alunos.length + 1,
+        nome,
+        cpf,
+        data_nascimento,
+        telefone,
+        email,
+        senha,
+        turma
+    };
+    alunos.push(novoAluno);
+
+    return res.status(201).json({mensagem:'Aluno criado com sucesso!'});
+};
 
 module.exports = {
     listarAlunos,
